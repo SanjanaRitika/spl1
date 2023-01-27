4fab0a5d3041c518e4b817090b7ed202390e170f
SanjanaRitika <120978695+SanjanaRitika@users.noreply.github.com>
2023-01-27 14:34:42 +0600

    Add files via upload

---
+++ New project 2.cpp
@@ -0,0 +1,126 @@
+//
+// Created by Ritika on 1/27/2023.
+//
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void clear_screen()
+{
+    system("@cls||clear");
+}
+
+typedef struct date_t
+{
+    int day;
+    int month;
+    int year;
+} date_t;
+
+typedef struct item_t
+{
+    int id;
+    char name[30];
+    int quantity;
+    float cost;
+    date_t mfg;
+    date_t exp;
+    char category[100];
+} item_t;
+
+void main_menu();
+void add_item();
+int check_id(int id, int count);
+void check_date(int day,int month,int year);
+
+void view_items();
+void view_options();
+void sort_alphabetically();
+void sort_id();
+
+void delete_menu();
+void delete_by_id();
+void delete_by_name();
+void delete_by_date();
+
+int search_by_id(item_t items[],int id,int count);
+int search_by_name(item_t items[],char *name,int count);
+int search_by_date(item_t items[],int day,int month,int year, int count);
+
+void modify_cost();
+void modify_options();
+void modify_qty();
+
+int main()
+{
+    printf("\t\tWelcome to Sanjana's INVENTORY MANAGEMENT SYSTEM\n\n");
+    main_menu();
+}
+
+void main_menu()
+{
+    int choice;
+    printf("\n");
+    printf("\nEnter option\n\n");
+    printf("1.Add Product\n");
+    printf("2.Delete Product\n");
+    printf("3.Modify Product\n");
+    printf("4.View products\n");
+    printf("5.Exit");
+    printf("\n\nEnter Choice\n");
+    scanf("%d",&choice);
+
+    switch(choice)
+    {
+        case 1: add_item();
+            break;
+        case 2: delete_menu();
+            break;
+        case 3: modify_options();
+            break;
+        case 4: view_options();
+            break;
+        case 5: exit(0);
+            break;
+        default:
+            printf("Invalid Choice!!!");
+            main_menu();
+
+    }
+
+}
+
+
+
+
+
+
+void view_options()
+{
+    clrscr();
+
+    int user_choice;
+    ILE *file_pointer=fopen("counter.dat","r");
+    int item_count;
+    fscanf(file_pointer,"%d",&item_count);
+    printf("\n\t\tTotal number of items in inventory: %d\n",item_count);
+
+    printf("\n1. View items by product number");
+    printf("\n2. View items alphabetically ");
+    printf("\n3. View items by last added");
+    printf("\n\nEnter option: ");
+    scanf("%d",&user_choice);
+
+    switch(user_choice)
+    {
+      case 1: sort_by_product_number();
+      break;
+      case 2: sort_alphabetically();
+      break;
+      case 3: view();
+      break;
+      default: menu();
+
+    }
+
+}
